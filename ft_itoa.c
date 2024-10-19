f22a46cb687d09a41cea65cd71d19dd9356b578e
Caroline Caleguer <carolineyamashitac@gmail.com>
2024-10-19 12:28:31 -0300

    ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,29 +1,41 @@
 
 #include "libft.h"
 
-int		ft_convitoa(size_t num, char *nbr)
+int		ft_convitoa(size_t num, char *nbr, int i)
 {
-	int i;
-	if (num < 9)
-		i = 0;
-	else
-		i = ft_convitoa((num / 10), nbr);
+	if (num > 9)
+		i = ft_convitoa((num / 10), nbr, i);
 	nbr[i] = (num % 10) + '0';
 	return (i + 1);
 }
 char	*ft_itoa(int n)
 {
-    char	*nbr;
+    int     i;
 	long	num;
+    char	*nbr;
 
-	nbr = malloc(11 * sizeof(char));
+    i = 0;
 	num = n;
-	ft_convitoa(num, nbr);
+	nbr = malloc(12 * sizeof(char));
+    if (num < 0)
+    {
+        num = -num;
+        i = 1;
+        nbr[0] = '-';
+    }
+	i = ft_convitoa(num, nbr, i);
+    nbr[i] = '\0';
 	return (nbr);
 }
 
 int		main(void)
 {
-	printf("%s", ft_itoa(67));
+	printf("%s\n", ft_itoa(-67));
+	printf("%s\n", ft_itoa(INT_MIN));
+	printf("%s\n", ft_itoa(INT_MAX));
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(-357));
+	//printf("%s\n", ft_itoa(61671684174861));
+	//printf("%s\n", ft_itoa(-61671684174861));
 	return (0);
 }
--- libft.h
+++ libft.h
@@ -19,6 +19,7 @@
 # include <stdio.h> //del
 # include <unistd.h>
 # include <fcntl.h>
+# include <limits.h> //del
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -46,6 +47,7 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n)
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
